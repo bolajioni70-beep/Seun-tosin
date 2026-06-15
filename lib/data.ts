7cc6115aae827efb51cbdc5f35eade27a8444593
bolajioni70-beep <bolajioni70@gmail.com>
2026-06-15 18:09:34 +0100

    Create data.ts

---
+++ lib/data.ts
@@ -0,0 +1,15 @@
+// lib/data.ts
+
+export const loveStory = `
+We met by chance, grew by choice, and now we celebrate love eternal.
+`;
+
+export const dressCode = `
+Elegant traditional attire in maroon and gold.
+`;
+
+export const rsvpOptions = ["Yes, I will attend", "No, I cannot make it"];
+
+export const giftMessage = `
+Your presence is the greatest gift. If you wish to bless us further, kindly see our gift section.
+`;
